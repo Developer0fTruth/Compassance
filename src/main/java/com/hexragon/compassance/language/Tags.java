6bab985ace3b5a091ddc4efbbd6d824697ef76ef
Dracomeister <dracomeisterat@gmail.com>
2016-04-05 20:03:51 -0500

    Final update

--- src/main/java/com/hexragon/compassance/language/Tags.java
+++
@@ -1,7 +0,0 @@
-package com.hexragon.compassance.language;
-
-public class Tags
-{
-    public static final String prefix = "&9&lCOMPASS &8» ";
-    public static final String usage = "&9&lUSAGE &8» ";
-}
